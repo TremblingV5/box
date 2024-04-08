6c11c8b4966bef21de3f7181c829cb0678e9cef9
TremblingV5 <1340349413@qq.com>
2024-04-08 12:05:48 +0800

    update

--- configx/config.go
+++ configx/config.go
@@ -1,6 +1,7 @@
 package configx
 
 import (
+	"fmt"
 	"github.com/spf13/viper"
 	"os"
 )
@@ -19,39 +20,39 @@ func SetRootConfigPath(path string) {
 }
 
 type Config struct {
-	System           *SystemConfig        `yaml:"system"`
-	SubConfig        map[string]SubConfig `yaml:"subConfig"`
-	ComponentLoadMap ComponentLoadMap     `yaml:"componentLoadMap"`
+	System           *SystemConfig        `yaml:"system" json:"system" mapstructure:"system"`
+	SubConfig        map[string]SubConfig `yaml:"subConfig" json:"subConfig" mapstructure:"subConfig"`
+	ComponentLoadMap ComponentLoadMap     `yaml:"componentLoadMap" json:"componentLoadMap" mapstructure:"componentLoadMap"`
 }
 
 type SystemConfig struct {
-	AppName      string `yaml:"appName"`
-	RouterPrefix string `yaml:"routerPrefix"`
-	Host         string `yaml:"host"`
-	HttpPort     int    `yaml:"httpPort"`
-	GrpcPort     int    `yaml:"grpcPort"`
-	Mode         string `yaml:"mode"`
-	Env          string `yaml:"env"`
-	EnablePprof  bool   `yaml:"enablePprof"`
+	AppName      string `yaml:"appName" json:"appName" mapstructure:"appName"`
+	RouterPrefix string `yaml:"routerPrefix" json:"routerPrefix" mapstructure:"routerPrefix"`
+	Host         string `yaml:"host" json:"host" mapstructure:"host"`
+	HttpPort     int    `yaml:"httpPort" json:"httpPort" mapstructure:"httpPort"`
+	GrpcPort     int    `yaml:"grpcPort" json:"grpcPort" mapstructure:"grpcPort"`
+	Mode         string `yaml:"mode" json:"mode" mapstructure:"mode"`
+	Env          string `yaml:"env" json:"env" mapstructure:"env"`
+	EnablePprof  bool   `yaml:"enablePprof"	json:"enablePprof" mapstructure:"enablePprof"`
 }
 
 type SubConfig struct {
-	Type     string `yaml:"type"` // inherit | file | viper
-	Location string `yaml:"location"`
+	Type     string `yaml:"type" json:"type" mapstructure:"type"` // inherit | file | viper
+	Location string `yaml:"location" json:"location" mapstructure:"location"`
 }
 
 type TraceConfig struct {
-	Enable  bool   `yaml:"enable"`
-	Type    string `yaml:"type"`
-	Address string `yaml:"address"`
+	Enable  bool   `yaml:"enable" json:"enable" mapstructure:"enable"`
+	Type    string `yaml:"type" json:"type" mapstructure:"type"`
+	Address string `yaml:"address" json:"address" mapstructure:"address"`
 }
 
 type ComponentLoadMap map[string]*ComponentLoadConfig
 
 type ComponentLoadConfig struct {
-	Disable   bool   `yaml:"disable"`
-	StoreKey  string `yaml:"storeKey"`
-	ConfigKey string `yaml:"configKey"`
+	Disable   bool   `yaml:"disable" json:"disable" mapstructure:"disable"`
+	StoreKey  string `yaml:"storeKey" json:"storeKey" mapstructure:"storeKey"`
+	ConfigKey string `yaml:"configKey" json:"configKey" mapstructure:"configKey"`
 }
 
 func Load(value any) error {
@@ -70,6 +71,8 @@ func loadFromYaml(value any) error {
 	v.SetConfigFile(RootConfigPath)
 	v.SetConfigType("yaml")
 	v.ReadInConfig()
+	temp := v.Get("componentLoad")
+	fmt.Println(temp)
 	err := v.Unmarshal(value)
 	return err
 }
